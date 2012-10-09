3bee2a75f7e3e11d0881851b51bb112db3f98f84
Hristo Deshev <hristo@deshev.com>
2012-10-09 14:25:00 +0000

    chown the WEB_ROOT dir to our PHP_USER/PHP_GROUP

--- install.php
+++ install.php
@@ -5,6 +5,7 @@ require_once("lib/fpm.php");
 require_once("lib/nginx.php");
 
 run("mkdir -p \"". WEB_ROOT . "\"");
+run("chown " . PHP_USER . ":" . PHP_GROUP . " -R \"" . WEB_ROOT . "\"");
 configure_fpm();
 configure_nginx();
 ?>
